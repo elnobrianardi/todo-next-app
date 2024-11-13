304068c8ec3363878e711b2831c8baca620931fd
elnobrianardi <elno.brianardi@gmail.com>
2024-11-13 12:48:54 +0700

    2nd commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <div className='flex py-3 flex-wrap justify-around'>
+            <h1 className='text-lg font-semibold'>To do APP</h1>
+            <ul className='flex gap-[40px] text-m'>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
